dd5e365e87f6125333cc240132b5b739224090e3
ronak sheth <ronak.sheth@sheridancollege.ca>
2022-01-21 09:35:07 -0500

    first commit

---
+++ src/student_w22_fri/Student.java
@@ -0,0 +1,35 @@
+
+package student_w22_fri;
+
+
+public class Student {
+    private String studentId;
+    private String studentName;
+
+    public Student(String studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+  
+    
+    
+    
+    
+}
---
+++ src/student_w22_fri/StudentList.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student_w22_fri;
+
+/**
+ *
+ * @author Ronak
+ */
+public class StudentList {
+    public static void main(String[] args) {
+        
+        Student[] studentList = new Student[3];
+        
+        studentList[0]= new Student("s1","Ronak Sheth");
+        studentList[1]= new Student("s2","john doe");
+        studentList[2]= new Student("s3","abc");
+        
+        for(int i=0;i<studentList.length;i++){
+            System.out.println(studentList[i].getStudentName());
+        }
+        
+    }
+}
